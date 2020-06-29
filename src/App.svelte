cd55a1619a8b5827005a27d68656e2c040662fa9
nick_richardson <nick.richardson@rezonence.com>
2020-06-29 13:48:37 +0100

    Set up svelte with ts

---
+++ src/App.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	let num: number = 42;
+	import {thing} from "./test";
+</script>
+
+<main>
+	<h1>Hello {thing}!</h1>
+	<p>Visit the {num}</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
